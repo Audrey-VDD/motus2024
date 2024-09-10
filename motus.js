162184190674bc0a395a7f0d40b785d53929eb45
Audrey Vandendriessche <avandendriessche1986@gmail.com>
2024-09-10 16:59:58 +0200

    Fonction clavier click et keydown

--- motus.js
+++ motus.js
@@ -3,7 +3,22 @@ document.addEventListener('DOMContentLoaded',()=>{
     const generateurMot = Math.floor(Math.random()* tableauMots.length);
 
     console.log(tableauMots[generateurMot]);
-    
+
+    const letter = document.getElementsByClassName("letter");
+
+
+    // Fonction pour les touches clavier
+    for(let i=0;i<letter.length;i++){
+        letter[i].addEventListener("click",()=>{
+            console.log(letter[i].innerText);
+        })
+    }
+
+    document.addEventListener('keydown', (e)=> {
+        console.log(e.key);
+        // afficher dans le tableau
+        
+    })
 
 
 
