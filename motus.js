2d553fc554be93d629849f91067f12ae2e8ff7d7
Audrey Vandendriessche <avandendriessche1986@gmail.com>
2024-09-10 15:50:05 +0200

    Creation visuel

--- motus.js
+++ motus.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded',()=>{
+    const tableauMots = ["cache", "codes", "liens", "octets", "cookie", "emuler", "binaire", "curseur", "domaine", "logiciel", "protocol", "terminal"];
+    const generateurMot = Math.floor(Math.random()* tableauMots.length);
+
+    console.log(tableauMots[generateurMot]);
+    
 
 
 
