0a9c93c4ca06b0ebf28aa71cdca1c9622144700d
Audrey Vandendriessche <avandendriessche1986@gmail.com>
2024-09-11 10:38:22 +0200

    Update JS, HTML

--- motus.js
+++ motus.js
@@ -1,8 +1,43 @@
 document.addEventListener('DOMContentLoaded',()=>{
     const tableauMots = ["cache", "codes", "liens", "octets", "cookie", "emuler", "binaire", "curseur", "domaine", "logiciel", "protocol", "terminal"];
     const generateurMot = Math.floor(Math.random()* tableauMots.length);
+    let motAffiche = tableauMots[generateurMot].split('');
+    const nombreTentatives = 6;
+    const supprimer = document.getElementById("btnSuppr");
+    const valider = document.getElementById("btnValider");
 
     console.log(tableauMots[generateurMot]);
+    console.log(motAffiche);
+    console.log(motAffiche[1]);
+    
+
+    let table = document.getElementById("myTable");
+    // insert une ligne
+    let nouvelleLigne = table.insertRow(0);
+
+    // insert une cellule
+    let nouvelleCellule = nouvelleLigne.insertCell(0);
+    // let mot = document.createTextNode(tableauMots[generateurMot][0]);
+    // nouvelleCellule.appendChild(mot);
+
+
+
+
+    for(let y=0; y<motAffiche.length;y++){
+        let nouvelleCellule = nouvelleLigne.insertCell();
+        y = document.createTextNode(motAffiche[y]);
+        nouvelleCellule.appendChild(y);
+    }
+
+
+
+
+    // Trouver la fonction pour afficher le mot sur le html
+    for(let i=1; i<tableauMots[generateurMot].length;i++){
+        motAffiche.innerText += "_";
+        console.log(motAffiche);        
+    }
+
 
     const letter = document.getElementsByClassName("letter");
 
@@ -18,10 +53,7 @@ document.addEventListener('DOMContentLoaded',()=>{
         console.log(e.key);
         // afficher dans le tableau
         
-    })
-
-
-
+    });
 
 
     
