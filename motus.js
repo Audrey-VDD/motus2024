c753eeddc29ceebd94a639d0a80f58eb6078af00
Mike-design59152 <m.collin59@outlook.fr>
2024-09-11 15:30:38 +0200

    fonction mot masqué

--- motus.js
+++ motus.js
@@ -1,84 +1,51 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const tableauMots = ["cache", "codes", "liens", "octets", "cookie", "emuler", "binaire", "curseur", "domaine", "logiciel", "protocol", "terminal"];
-    const generateurMot = Math.floor(Math.random() * tableauMots.length);
-    let motAffiche = tableauMots[generateurMot];
+document.addEventListener("DOMContentLoaded", () => {
+  const tableauMots = [
+    "cache",
+    "codes",
+    "liens",
+    "octets",
+    "cookie",
+    "emuler",
+    "binaire",
+    "curseur",
+    "domaine",
+    "logiciel",
+    "protocol",
+    "terminal",
+  ];
+  const generateurMot = Math.floor(Math.random() * tableauMots.length);
+  let motAffiche = tableauMots[generateurMot];
 
-    // créer variable mot splité
-    let motSecret = motAffiche.split('');
-    const nombreTentatives = 6;
-    const supprimer = document.getElementById("btnSuppr");
-    const valider = document.getElementById("btnValider");
+  let motAfficheMasque = motAffiche
+    .split("")
+    .map((lettre, index) => (index === 0 ? lettre : "."))
+    .join("");
+  // créer variable mot splité
+  let motSecret = motAffiche.split("");
+  const nombreTentatives = 6;
+  const supprimer = document.getElementById("btnSuppr");
+  const valider = document.getElementById("btnValider");
+  console.log(motAffiche);
+  const tableau = document.getElementById("myTable");
 
-
-
-    console.log(motAffiche);
-
-
-
-    const tableau = document.getElementById("myTable");
-
-    for (i = 1; i < nombreTentatives; i++) {
-        const ligne = document.createElement('tr');
-        motSecret.forEach(() => {
-            // créer une td dans le tr
-            const cellule = document.createElement('td');
-            cellule.textContent = "_";
-
-
-        ligne.appendChild(cellule);
-
-
-
-
-        });
-        tableau.appendChild(ligne);
-    }
-
-
-
-    // insert une ligne
-    // const ligne = document.createElement('tr');
-    // motSecret.forEach((lettre, index)=>{
-    //     // créer une td dans le tr
-    //     const cellule = document.createElement('td');
-    //     if(index === 0){
-    //         cellule.textContent = "_";
-    //     }
-    //     ligne.appendChild(cellule);
-    // });
-    // tableau.appendChild(ligne);
-
-
-
-
-
-
-
-
-
-
-
-    const letter = document.getElementsByClassName("letter");
-    // Fonction pour les touches clavier
-    for (let i = 0; i < letter.length; i++) {
-        letter[i].addEventListener("click", () => {
-            console.log(letter[i].innerText);
-        })
-    }
-
-    document.addEventListener('keydown', (e) => {
-        console.log(e.key);
-        // afficher dans le tableau  
+  for (let u = 0; u < nombreTentatives; u++) {
+    const ligne = document.createElement("tr");
+    motAfficheMasque.split("").forEach((caractere) => {
+      const cellule = document.createElement("td");
+      cellule.textContent = caractere; // Affiche le caractère masqué
+      ligne.appendChild(cellule);
     });
-
-
-
-
-
-
-
-
-
-
-
-})
+    tableau.appendChild(ligne);
+  }
+  const letter = document.getElementsByClassName("letter");
+  // Fonction pour les touches clavier
+  for (let i = 0; i < letter.length; i++) {
+    letter[i].addEventListener("click", () => {
+      console.log(letter[i].innerText);
+    });
+  }
+  document.addEventListener("keydown", (e) => {
+    console.log(e.key);
+    // afficher dans le tableau
+  });
+});
