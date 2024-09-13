ffce2cfa900ba0488dfa8eb6ceec04ee575ff947
Audrey Vandendriessche <avandendriessche1986@gmail.com>
2024-09-13 08:58:09 +0200

    mise à jour avec alert

--- motus.js
+++ motus.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+
     const tableauMots = [
         "cache",
         "codes",
@@ -14,38 +15,47 @@ document.addEventListener("DOMContentLoaded", () => {
         "terminal",
     ];
 
+    // Variable qui sélectionne un mot
     const generateurMot = Math.floor(Math.random() * tableauMots.length);
     let motAffiche = tableauMots[generateurMot];
 
+    // splite le mot, affiche que la première lettre 0
     let motAfficheMasque = motAffiche
         .split("")
         .map((lettre, index) => (index === 0 ? lettre : "-"))
         .join("");
-    // créer variable mot splité
 
+    
     const nombreTentatives = 6;
     const supprimer = document.getElementById("btnSuppr");
     const valider = document.getElementById("btnValider");
     const tableau = document.getElementById("myTable");
 
-    console.log(motAffiche);
+    alert("Deviner le bon mot. \n \n Rouge = lettre bien placée. \n Orange = lettre présente mais mal placée. \n Gris = lettre non présente dans le mot. \n \n Bonne chance !");
 
-
-
+    // Boucle pour nos 6 tentatives
     for (let u = 0; u < nombreTentatives; u++) {
+
         const ligne = document.createElement("tr");
+        // pour chaque lettre du mot splité, tu créés un td et tu me l'affiches dans cellule créée
         motAfficheMasque.split("").forEach((caractere) => {
             const cellule = document.createElement("td");
-            cellule.textContent = caractere; // Affiche le caractère masqué
+            cellule.textContent = caractere;
             ligne.appendChild(cellule);
         });
-        tableau.appendChild(ligne);
+
+        // création bouton jouer pour faire apparaître le tableau
+        let jouer = document.getElementById("jouer");
+        jouer.addEventListener('click', () => {
+            tableau.appendChild(ligne);
+        })
     };
 
 
     // fonction pour que la lettre se mette dans la cellule 1 ligne 0
     let choixJoueur = 0;
     let positionLigne = 1;
+    
 
 
 
@@ -54,64 +64,85 @@ document.addEventListener("DOMContentLoaded", () => {
         const cellules = ligne.getElementsByTagName("td");
 
 
-        
 
-        if (positionLigne < cellules.length) {
+        // rajout de égal pour que même si on clique sur valider avant la fin de la ligne
+        //  on ne passe pas à la ligne du dessous
+        if (positionLigne <= cellules.length) {
             cellules[positionLigne].textContent = lettre; // Insère la lettre dans la cellule courante
 
-          let motCherche = motAffiche.replace(motAffiche[0],"");
+            // Echange première lettre du mot contre rien
+            let motCherche = motAffiche.replace(motAffiche[0], "");
 
 
-          if(lettre===motAffiche[positionLigne]){
-            cellules[positionLigne].classList.add("bien-place");
-            // appliquer le css dans le clavier du HTML
-            // verrouiller la lettre
-            // reprendre la lettre ligne de dessous
-            
-            let lettreTrouve = document.getElementsByClassName("bien-place");
-            console.log(lettreTrouve);
-          
+            if (lettre === motAffiche[positionLigne]) {
+                cellules[positionLigne].classList.add("bien-place");
+                // appliquer le css dans le clavier du HTML
+                // verrouiller la lettre
+                // reprendre la lettre ligne de dessous
+
+
+                // Test pour trouver la lettre qui est en rouge
+                let lettreTrouve = document.getElementsByClassName("bien-place");
+                console.log(lettreTrouve);
 
 
 
-          } else if (motCherche.includes(lettre)){
-              cellules[positionLigne].classList.add("mal-place");
-           // appliquer le css dans le clavier du HTML
-          }
-          // si la lettre n'est pas dans le mot
-          else{
-            cellules[positionLigne].classList.add("incorrect");
-          };
-          
-          positionLigne++; // Avance à la prochaine cellule;
+
+            } else if (motCherche.includes(lettre)) {
+                cellules[positionLigne].classList.add("mal-place");
+                // appliquer le css dans le clavier du HTML
+            }
+            // si la lettre n'est pas dans le mot
+            else {
+                cellules[positionLigne].classList.add("incorrect");
+            };
+
+            positionLigne++; // Avance à la prochaine cellule
         };
-        
-        
-        
-        
-        
-        if(positionLigne === cellules.length){
-          choixJoueur++;
-          positionLigne = 1;
 
-        }
-    };
+        valider.addEventListener('click', ()=>{
+            if(positionLigne === cellules.length){
+                choixJoueur++;
+                positionLigne = 1;
+            }
+        })
+        document.addEventListener("keydown", (e) => {
+            if(e.key === 'Enter'){
+                choixJoueur++;
+                positionLigne = 1; 
+            }
+        })
+
+    }
+
+
+
+
+
+
+
 
     // Fonction pour les touches clavier html
     const letter = document.getElementsByClassName("letter");
 
+
+    // couleurs ne fonctionnent pas
     for (let i = 0; i < letter.length; i++) {
         letter[i].addEventListener("click", () => {
-            const lettre = letter[i].innerText
+            let lettre = letter[i].innerText
             saisi(lettre);
+            console.log(lettre);
         });
     };
 
-    //Pour les touches clavier pc
+    //Pour les touches clavier pc ok pour les couleurs
     document.addEventListener("keydown", (e) => {
-        const keybord = e.key;
+        let keybord = e.key;
         saisi(keybord);
     });
+
+
+    
 
 
 
