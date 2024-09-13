e54d0e55498a657731be975c43ee760dddb373d9
Jordan <jordan.valente@hotmail.fr>
2024-09-13 16:24:17 +0200

    fonction supp

--- motus.js
+++ motus.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded"){
     const tableauMots = [
         "cache",
         "codes",
@@ -17,25 +17,29 @@ document.addEventListener("DOMContentLoaded", () => {
     const generateurMot = Math.floor(Math.random() * tableauMots.length);
     let motAffiche = tableauMots[generateurMot];
 
-    // Initialisation du mot masqué
+    // split le mot choisit
     let motAfficheMasque = motAffiche
         .split("")
         .map((lettre, index) => (index === 0 ? lettre : "-"))
         .join("");
+
+    // nmbre tentatives = nombre delignes
 
     const nombreTentatives = 6;
     const supprimer = document.getElementById("btnSuppr");
     const valider = document.getElementById("btnValider");
     const tableau = document.getElementById("myTable");
 
+    // Ouverture de la page, alert
+
     alert(
         "Deviner le bon mot. \n \n Rouge = lettre bien placée. \n Orange = lettre présente mais mal placée. \n Gris = lettre non présente dans le mot. \n \n Bonne chance !"
     );
 
-    console.log(motAffiche);
 
     // Créer le tableau pour les essais
     const lignes = [];
+    // Array qui stocke nos bonnes lettres
     const lettresBienPlacees = Array(motAffiche.length).fill(null); // Tableau pour stocker les lettres bien placées
     for (let u = 0; u < nombreTentatives; u++) {
         const ligne = document.createElement("tr");
@@ -44,36 +48,40 @@ document.addEventListener("DOMContentLoaded", () => {
             cellule.textContent = index === 0 ? caractere : "-"; // Affiche la lettre bien placée ou le caractère masqué
             ligne.appendChild(cellule);
         });
+
+        // touche jouer fait apparaitre le tableau
         let jouer = document.getElementById("jouer");
         jouer.addEventListener("click", () => {
             tableau.appendChild(ligne);
         });
 
-        lignes.push(ligne); // Ajouter la ligne à un tableau pour une utilisation future
+        // Tu affiches la ligne array dans la ligne du tableau
+        lignes.push(ligne);
     }
-
+    // la premiere ligne
     let choixJoueur = 0;
+
+    // la deuxieme ligne
     let positionLigne = 1;
 
+    // fonction pour les couleurs
     function saisi(lettre) {
         const ligne = lignes[choixJoueur];
         const cellules = ligne.getElementsByTagName("td");
-        const motActuel = motAffiche.split("");
 
-        console.log(lettre);
 
+        // Si tu n'es pas à la fin de la ligne
         if (positionLigne < cellules.length) {
-            if (positionLigne > 0) {
-                // Ne pas modifier la première cellule
+            if (positionLigne > 0) {// Ne pas modifier la première cellule
                 cellules[positionLigne].textContent = lettre; // Insère la lettre dans la cellule courante
 
                 // Vérifier si la lettre est correcte
                 if (lettre === motAffiche[positionLigne]) {
                     cellules[positionLigne].classList.add("bien-place");
-
                     lettresBienPlacees[positionLigne] = lettre; // Conserver la lettre bien placée
                 } else if (motAffiche.includes(lettre)) {
                     cellules[positionLigne].classList.add("mal-place");
+
                 } else {
                     cellules[positionLigne].classList.add("incorrect");
                 }
@@ -81,8 +89,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
             positionLigne++;
         }
+
         valider.addEventListener("click", () => {
+
             if (positionLigne === cellules.length) {
+
                 // Passer à la ligne suivante
                 choixJoueur++;
                 positionLigne = 1;
@@ -97,47 +108,60 @@ document.addEventListener("DOMContentLoaded", () => {
                                 (index === 0 ? motAffiche[index] : "-");
                         });
                 }
+
             }
-        });
-    }
 
-    // fonction pour supprimer la dernière lettre saisie
+            let rouge = ligne.getElementsByClassName("bien-place");
+            if ((rouge.length + 1) === motAffiche.length) {
+                alert("Victoire")
+            } else {
 
-    function supprimerDerniereLettre() {
-        const ligne = lignes[choixJoueur];
-        const cellules = ligne.getElementsByTagName("td");
-        if (positionLigne > 1) {
-            positionLigne--;
-            cellules[positionLigne].classList.remove("bien-place");
-            cellules[positionLigne].classList.remove("mal-place");
-            cellules[positionLigne].classList.remove("incorrect");
-            cellules[positionLigne].textContent = "-";
+            }
+        })
+
+
+
+
+        // fonction pour supprimer la dernière lettre saisie
+
+        function supprimerDerniereLettre() {
+            const ligne = lignes[choixJoueur];
+            const cellules = ligne.getElementsByTagName("td");
+            if (positionLigne > 1) {
+                positionLigne--;
+                cellules[positionLigne].classList.remove("bien-place");
+                cellules[positionLigne].classList.remove("mal-place");
+                cellules[positionLigne].classList.remove("incorrect");
+                cellules[positionLigne].textContent = "-";
+            }
         }
-    }
 
-    supprimer.addEventListener("click", () => {
-        supprimerDerniereLettre();
-    });
-
-    // Fonction pour les touches clavier html
-    const letter = document.getElementsByClassName("letter");
-
-    for (let i = 0; i < letter.length; i++) {
-        letter[i].addEventListener("click", () => {
-            const lettre = letter[i].innerText.toLowerCase();
-            saisi(lettre);
+        supprimer.addEventListener("click", () => {
+            supprimerDerniereLettre();
         });
-    }
 
-    // Pour les touches clavier PC
-    document.addEventListener("keydown", (e) => {
-        const keybord = e.key.toLowerCase();
-        if (keybord.length === 1 && keybord.match(/[a-z]/)) {
-            saisi(keybord);
-        } else if (keybord === "backspace") {
-            supprimerDerniereLettre();
-        } else if (keybord === "delete") {
-            supprimerDerniereLettre();
+        // Fonction pour les touches clavier html
+        const letter = document.getElementsByClassName("letter");
+
+        for (let i = 0; i < letter.length; i++) {
+            letter[i].addEventListener("click", () => {
+                const lettre = letter[i].innerText.toLowerCase();
+                saisi(lettre);
+            });
         }
-    });
-});
+
+        // Pour les touches clavier PC
+        document.addEventListener("keydown", (e) => {
+            const keybord = e.key.toLowerCase();
+            if (keybord.length === 1 && keybord.match(/[a-z]/)) {
+                saisi(keybord);
+            } else if (keybord === "backspace") {
+                supprimerDerniereLettre();
+            } else if (keybord === "delete") {
+                supprimerDerniereLettre();
+            }
+
+        });
+    };
+
+}
